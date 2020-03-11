afcac552c4dbefe9fde1b331cec6aee74065c683
Sarah Gibson <sgibson@turing.ac.uk>
2020-03-11 11:32:35 +0000

    [DEV] Begin constructing UpdateDockerTags class

---
+++ UpdateDockerTags/UpdateDockerTags.py
@@ -0,0 +1,10 @@
+class UpdateDockerTags:
+    """Class to check that the tags of Docker images in a JupyterHub config
+    are up to date"""
+
+    def __init__(self):
+        """Constructor function for UpdateDockerTags class"""
+
+
+if __name__ == "__main__":
+    obj = UpdateDockerTags()
