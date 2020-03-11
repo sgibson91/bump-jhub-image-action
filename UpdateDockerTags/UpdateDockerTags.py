7adf371ce64e9c84d28f041d4bfe6994a2b6a9c3
Sarah Gibson <sgibson@turing.ac.uk>
2020-03-11 13:50:32 +0000

    [DEV] Updating naming convention

--- UpdateDockerTags/UpdateDockerTags.py
+++ UpdateDockerTags/UpdateDockerTags.py
@@ -25,31 +25,30 @@ class UpdateDockerTags:
         """Function to check the image tags against the currently deployed tags
         """
         self.new_image_tags = {}
-        self.api_urls = {
+        self.old_image_tags = {}
+
+        api_urls = {
             "jupyterhub": f"https://raw.githubusercontent.com/{self.repo_owner}/{self.repo_name}/master/config/config-template.yaml",
             "minimal-notebook": "https://hub.docker.com/v2/repositories/jupyter/minimal-notebook/tags",
             "datascience-notebook": "https://hub.docker.com/v2/repositories/jupyter/datascience-notebook/tags",
             "custom-env": f"https://hub.docker.com/v2/repositories/{self.docker_repo}/{self.docker_image}/tags",
         }
+        images = list(api_urls.keys())
+        images.remove("jupyterhub")
 
-        self.find_most_recent_tag_github(self.api_urls["jupyterhub"])
+        self.find_most_recent_tag_github(api_urls["jupyterhub"])
 
-        self.api_urls.pop("jupyterhub", None)
-        for api_url in self.api_urls:
-            self.find_most_recent_tag_dockerhub(
-                api_url, self.api_urls[api_url]
-            )
+        for image in images:
+            self.find_most_recent_tag_dockerhub(image, api_urls[image])
 
         cond = [
-            (
-                self.old_image_tags[image_name]
-                != self.new_image_tags[image_name]
-            )
-            for image_name in self.api_urls.keys()
+            (self.old_image_tags[image] != self.new_image_tags[image])
+            for image in images
         ]
 
         if np.any(cond):
             print("Some images need updating")
+            # self.update_images(list(compress(images, condition)))
         else:
             print("All images are up to date")
 
@@ -60,10 +59,10 @@ class UpdateDockerTags:
             name {str} -- Name of the image
             url {str} -- API URL of the Docker Hub image repository
         """
-        results = json.loads(requests.get(url).text)
+        res = json.loads(requests.get(url).text)
 
         updates_sorted = sorted(
-            results["results"], key=lambda k: k["last_updated"]
+            res["results"], key=lambda k: k["last_updated"]
         )
 
         if updates_sorted[-1]["name"] == "latest":
@@ -79,26 +78,24 @@ class UpdateDockerTags:
         Arguments:
             url {str} -- GitHub raw content URL to read config from
         """
-        self.old_image_tags = {}
-
         headers = {"Authorization": f"token {API_TOKEN}"}
-        results = yaml.safe_load(requests.get(url, headers=headers).text)
+        res = yaml.safe_load(requests.get(url, headers=headers).text)
 
-        self.old_image_tags["minimal-notebook"] = results["singleuser"][
-            "image"
-        ]["tag"]
+        self.old_image_tags["minimal-notebook"] = res["singleuser"]["image"][
+            "tag"
+        ]
 
         for image_name in ["datascience-notebook", "custom-env"]:
-            for i in results["singleuser"]["profileList"]:
+            for profile in res["singleuser"]["profileList"]:
                 datascience_cond = (image_name == "datascience-notebook") and (
-                    i["display_name"] == "Data Science Environment"
+                    profile["display_name"] == "Data Science Environment"
                 )
                 custom_cond = (image_name == "custom-env") and (
-                    i["display_name"] == "Custom repo2docker image"
+                    profile["display_name"] == "Custom repo2docker image"
                 )
 
                 if datascience_cond or custom_cond:
-                    old_image = i["kubespawner_override"]["image"]
+                    old_image = profile["kubespawner_override"]["image"]
                     old_tag = old_image.split(":")[-1]
                     self.old_image_tags[image_name] = old_tag
 
