048089924f63a50f98409ec9e845b4e99a059d6c
Sarah Gibson <drsarahlgibson@gmail.com>
2021-09-22 22:05:24 +0100

    Add docstrings to image tag parsing funcs

--- tag_bot/parse_image_tags.py
+++ tag_bot/parse_image_tags.py
@@ -15,6 +15,24 @@ def get_deployed_image_tags(
     image_tags: dict,
     config_type: str = "singleuser",
 ) -> dict:
+    """Read the tags of the deployed images from a YAML config file
+
+    Args:
+        repo_owner (str): The GitHub account that owns the host repo
+        repo_name (str): The name of the host repo
+        branch (str): The branch the config file is located on
+        filepath (str): The path to the config file
+        header (dict): A dictionary of headers to send with the API requests.
+            Must contain an authorisation token.
+        image_tags (dict): A dictionary to store the image information in
+        config_type (str, optional): Whether the config supports a single image
+            or multiple images via a profile list. Options are "singleuser" or
+            "profileList" (case sensitive). Defaults to "singleuser".
+
+    Returns:
+        image_tags (dict): A dictionary containing info on the images currently
+            deployed
+    """
     allowed_config_types = ["singleuser", "profileList"]
     if config_type not in allowed_config_types:
         raise NotImplementedError(
@@ -42,6 +60,16 @@ def get_deployed_image_tags(
 
 
 def get_most_recent_image_tags_dockerhub(image_name: str, image_tags: dict) -> dict:
+    """For an image hosted on DockerHub, look up the most recent tag
+
+    Args:
+        image_name (str): The name of the image to look up tags for
+        image_tags (dict): A dictionary to store the most recent tag in
+
+    Returns:
+        image_tags (dict): A dictionary containing info on the images and their
+            most recent tags
+    """
     url = "/".join([DOCKERHUB_ROOT, image_name, "tags"])
     resp = get_request(url, output="json")
 
@@ -60,6 +88,21 @@ def get_most_recent_image_tags_dockerhub(image_name: str, image_tags: dict) -> d
 def get_image_tags(
     repo_owner: str, repo_name: str, branch: str, filepath: str, header: dict
 ) -> dict:
+    """Get the image names and tags that are deployed in a config file
+
+    Args:
+        repo_owner (str): The GitHub account that owns the host repo
+        repo_name (str): The name of the host repo
+        branch (str): The name of the branch on which the config file is located
+        filepath (str): The path to the config file relative to the root of the
+            repo
+        header (dict): A dictionary of headers to send with API requests.
+            Must contain an authorisation token.
+
+    Returns:
+        image_tags (dict): A dictionary containing the names and tags, both
+            currently deployed and most recently released
+    """
     image_tags = {}
 
     image_tags = get_deployed_image_tags(
