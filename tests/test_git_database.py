70aef5e6bcde99cb15b77dfdf030f688dd4d9d27
Sarah Gibson <drsarahlgibson@gmail.com>
2021-09-28 14:15:41 +0100

    Adding a test suite

---
+++ tests/test_git_database.py
@@ -0,0 +1,111 @@
+import yaml
+import base64
+
+from unittest.mock import patch
+
+from tag_bot.git_database import create_commit, create_ref, get_contents, get_ref
+
+test_url = "http://jsonplaceholder.typicode.com"
+test_header = {"Authorization": "token ThIs_Is_A_ToKeN"}
+
+
+def test_create_commit():
+    test_path = "config/test_config.yaml"
+    test_branch = "test_branch"
+    test_sha = "test_sha"
+    test_commit_msg = "This is a commit message"
+
+    test_contents = {"key1": "This is a test"}
+    test_contents = yaml.safe_dump(test_contents).encode("utf-8")
+    test_contents = base64.b64encode(test_contents)
+    test_contents = test_contents.decode("utf-8")
+
+    test_body = {
+        "message": test_commit_msg,
+        "content": test_contents,
+        "sha": test_sha,
+        "branch": test_branch,
+    }
+
+    with patch("tag_bot.git_database.put") as mocked_func:
+        create_commit(
+            test_url,
+            test_header,
+            test_path,
+            test_branch,
+            test_sha,
+            test_commit_msg,
+            test_contents,
+        )
+
+        assert mocked_func.call_count == 1
+        mocked_func.assert_called_with(
+            "/".join([test_url, "contents", test_path]),
+            json=test_body,
+            headers=test_header,
+        )
+
+
+def test_create_ref():
+    test_ref = "test_ref"
+    test_sha = "test_sha"
+
+    test_body = {"ref": f"refs/heads/{test_ref}", "sha": test_sha}
+
+    with patch("tag_bot.git_database.post_request") as mocked_func:
+        create_ref(test_url, test_header, test_ref, test_sha)
+
+        assert mocked_func.call_count == 1
+        mocked_func.assert_called_with(
+            "/".join([test_url, "git", "refs"]),
+            headers=test_header,
+            json=test_body,
+        )
+
+
+def test_get_contents():
+    test_path = "config/test_config.yaml"
+    test_ref = "test_ref"
+    test_query = {"ref": test_ref}
+
+    mock_get = patch(
+        "tag_bot.git_database.get_request",
+        return_value={
+            "download_url": "/".join([test_url, "download", test_path]),
+            "sha": "blob_sha",
+        },
+    )
+
+    with mock_get as mocked_func:
+        resp = get_contents(test_url, test_header, test_path, test_ref)
+
+        assert mocked_func.call_count == 1
+        mocked_func.assert_called_with(
+            "/".join([test_url, "contents", test_path]),
+            headers=test_header,
+            params=test_query,
+            output="json",
+        )
+        assert resp == {
+            "download_url": "/".join([test_url, "download", test_path]),
+            "sha": "blob_sha",
+        }
+
+
+def test_get_ref():
+    test_ref = "test_ref"
+
+    mock_get = patch(
+        "tag_bot.git_database.get_request", return_value={"object": {"sha": "sha"}}
+    )
+
+    with mock_get as mock1:
+        resp = get_ref(test_url, test_header, test_ref)
+
+        assert mock1.call_count == 1
+        mock1.assert_called_with(
+            "/".join([test_url, "git", "ref", "heads", test_ref]),
+            headers=test_header,
+            output="json",
+        )
+        assert resp == {"object": {"sha": "sha"}}
---
+++ tests/test_utils.py
@@ -0,0 +1,110 @@
+import pytest
+import responses
+
+from tag_bot.utils import delete_request, get_request, post_request
+
+test_url = "http://jsonplaceholder.typicode.com/"
+test_header = {"Authorization": "token ThIs_Is_A_ToKeN"}
+test_body = {"Payload": "Send this with the request"}
+
+
+@responses.activate
+def test_delete_request():
+    responses.add(responses.DELETE, test_url)
+
+    delete_request(test_url, headers=test_header)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == test_url
+
+
+@responses.activate
+def test_delete_request_exception():
+    responses.add(responses.DELETE, test_url, status=500)
+
+    with pytest.raises(RuntimeError):
+        delete_request(test_url, headers=test_header)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == test_url
+
+
+@responses.activate
+def test_get_request():
+    responses.add(responses.GET, test_url, status=200)
+
+    resp = get_request(test_url, headers=test_header)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == test_url
+    assert resp.ok
+
+
+@responses.activate
+def test_get_request_json():
+    responses.add(responses.GET, test_url, json={"Response": "OK"}, status=200)
+
+    resp = get_request(test_url, headers=test_header, output="json")
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == test_url
+    assert resp == {"Response": "OK"}
+
+
+@responses.activate
+def test_get_request_text():
+    responses.add(responses.GET, test_url, json={"Response": "OK"}, status=200)
+
+    resp = get_request(test_url, headers=test_header, output="text")
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == test_url
+    assert resp == '{"Response": "OK"}'
+
+
+def test_get_request_output_exception():
+    with pytest.raises(ValueError):
+        _ = get_request(test_url, headers=test_header, output="yaml")
+
+
+@responses.activate
+def test_get_request_url_exception():
+    responses.add(responses.GET, test_url, status=500)
+
+    with pytest.raises(RuntimeError):
+        _ = get_request(test_url, headers=test_header)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == test_url
+
+
+@responses.activate
+def test_post_request():
+    responses.add(responses.POST, test_url, status=200)
+
+    post_request(test_url, headers=test_header, json=test_body)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == test_url
+
+
+@responses.activate
+def test_post_request_return_json():
+    responses.add(responses.POST, test_url, json={"Request": "Sent"}, status=200)
+
+    resp = post_request(test_url, headers=test_header, json=test_body, return_json=True)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == test_url
+    assert resp == {"Request": "Sent"}
+
+
+@responses.activate
+def test_post_request_exception():
+    responses.add(responses.POST, test_url, status=500)
+
+    with pytest.raises(RuntimeError):
+        post_request(test_url, headers=test_header, json=test_body)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == test_url
