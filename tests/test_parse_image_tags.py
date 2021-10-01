b40d1b6e4488507769eda0a84ffcf2529b751399
Sarah Gibson <drsarahlgibson@gmail.com>
2021-10-01 11:15:49 +0100

    Write tests for get_image_tags func

--- tests/test_parse_image_tags.py
+++ tests/test_parse_image_tags.py
@@ -1,8 +1,9 @@
-# TODO: Write test for get_image_tags func
+import pytest
 
 from unittest.mock import patch
 
 from tag_bot.parse_image_tags import (
+    get_image_tags,
     get_deployed_image_tags,
     get_most_recent_image_tags_dockerhub,
 )
@@ -132,3 +133,91 @@ def test_get_most_recent_image_tags_dockerhub():
             output="json",
         )
         assert output_image_tags == expected_image_tags
+
+
+def test_get_image_tags():
+    branch = "test_branch"
+    filepath = "config/config.yaml"
+
+    mock_deployed_tags = patch(
+        "tag_bot.parse_image_tags.get_deployed_image_tags",
+        return_value={"image_owner/image_name": {"current": "image_tag"}},
+    )
+    mock_dockerhub_tags = patch(
+        "tag_bot.parse_image_tags.get_most_recent_image_tags_dockerhub",
+        return_value={
+            "image_owner/image_name": {
+                "current": "image_tag",
+                "latest": "new_image_tag",
+            }
+        },
+    )
+
+    expected_image_tags = {
+        "image_owner/image_name": {
+            "current": "image_tag",
+            "latest": "new_image_tag",
+        }
+    }
+
+    with mock_deployed_tags as mock1, mock_dockerhub_tags as mock2:
+        image_tags = get_image_tags(test_url, test_header, branch, filepath)
+
+        assert mock1.call_count == 1
+        mock1.assert_called_with(
+            test_url,
+            test_header,
+            branch,
+            filepath,
+            {},
+        )
+        assert mock2.call_count == 1
+        mock2.assert_called_with(
+            "image_owner/image_name",
+            mock1.return_value,
+        )
+        assert image_tags == expected_image_tags
+
+
+def test_get_image_tags_not_implemented_error():
+    branch = "test_branch"
+    filepath = "config/config.yaml"
+
+    mock_deployed_tags = patch(
+        "tag_bot.parse_image_tags.get_deployed_image_tags",
+        return_value={"quay.io/image_owner/image_name": {"current": "image_tag"}},
+    )
+
+    with mock_deployed_tags as mock1, pytest.raises(NotImplementedError):
+        _ = get_image_tags(test_url, test_header, branch, filepath)
+
+        assert mock1.call_count == 1
+        mock1.assert_called_with(
+            test_url,
+            test_header,
+            branch,
+            filepath,
+            {},
+        )
+
+
+def test_get_image_tags_value_error():
+    branch = "test_branch"
+    filepath = "config/config.yaml"
+
+    mock_deployed_tags = patch(
+        "tag_bot.parse_image_tags.get_deployed_image_tags",
+        return_value={"image_name": {"current": "image_tag"}},
+    )
+
+    with mock_deployed_tags as mock1, pytest.raises(ValueError):
+        _ = get_image_tags(test_url, test_header, branch, filepath)
+
+        assert mock1.call_count == 1
+        mock1.assert_called_with(
+            test_url,
+            test_header,
+            branch,
+            filepath,
+            {},
+        )
