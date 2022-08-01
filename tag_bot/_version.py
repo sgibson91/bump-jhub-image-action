7b5fece29f27f585b21c3d8d4ffe7280ce7375b0
Georgiana Dolocan <georgiana.dolocan@gmail.com>
2022-08-01 17:43:02 +0300

    Bump minor version

--- tag_bot/_version.py
+++ tag_bot/_version.py
@@ -7,5 +7,5 @@ Provides tag_bot version information.
 
 from incremental import Version
 
-__version__ = Version("tag_bot", 1, 2, 0)
+__version__ = Version("tag_bot", 1, 3, 0)
 __all__ = ["__version__"]
